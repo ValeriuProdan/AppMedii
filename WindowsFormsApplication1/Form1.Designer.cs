6e84659e517bf76182b6c59f1ef69442641d38ba
Valeriu <Valeriu@DESKTOP-N2U838D>
2017-10-31 23:05:19 +0200

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Sugestii = new System.Windows.Forms.Label();
+            this.Nota1 = new System.Windows.Forms.TextBox();
+            this.Nota2 = new System.Windows.Forms.TextBox();
+            this.Nota3 = new System.Windows.Forms.TextBox();
+            this.Nota4 = new System.Windows.Forms.TextBox();
+            this.Nota5 = new System.Windows.Forms.TextBox();
+            this.Nota6 = new System.Windows.Forms.TextBox();
+            this.Nota7 = new System.Windows.Forms.TextBox();
+            this.Nota8 = new System.Windows.Forms.TextBox();
+            this.Teza = new System.Windows.Forms.TextBox();
+            this.Medie = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.button1.Location = new System.Drawing.Point(248, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(46, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Note";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label2.Location = new System.Drawing.Point(12, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Teza";
+            // 
+            // Sugestii
+            // 
+            this.Sugestii.AutoSize = true;
+            this.Sugestii.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sugestii.ForeColor = System.Drawing.Color.LimeGreen;
+            this.Sugestii.Location = new System.Drawing.Point(12, 80);
+            this.Sugestii.Name = "Sugestii";
+            this.Sugestii.Size = new System.Drawing.Size(0, 26);
+            this.Sugestii.TabIndex = 3;
+            this.Sugestii.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Nota1
+            // 
+            this.Nota1.BackColor = System.Drawing.Color.LightGray;
+            this.Nota1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Nota1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota1.ForeColor = System.Drawing.Color.Blue;
+            this.Nota1.Location = new System.Drawing.Point(92, 9);
+            this.Nota1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.Nota1.Name = "Nota1";
+            this.Nota1.Size = new System.Drawing.Size(20, 22);
+            this.Nota1.TabIndex = 4;
+            this.Nota1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota2
+            // 
+            this.Nota2.BackColor = System.Drawing.Color.LightGray;
+            this.Nota2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Nota2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota2.ForeColor = System.Drawing.Color.Blue;
+            this.Nota2.Location = new System.Drawing.Point(118, 9);
+            this.Nota2.Name = "Nota2";
+            this.Nota2.Size = new System.Drawing.Size(20, 22);
+            this.Nota2.TabIndex = 5;
+            this.Nota2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota3
+            // 
+            this.Nota3.BackColor = System.Drawing.Color.LightGray;
+            this.Nota3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Nota3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota3.ForeColor = System.Drawing.Color.Blue;
+            this.Nota3.Location = new System.Drawing.Point(144, 9);
+            this.Nota3.Name = "Nota3";
+            this.Nota3.Size = new System.Drawing.Size(20, 22);
+            this.Nota3.TabIndex = 6;
+            this.Nota3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota4
+            // 
+            this.Nota4.BackColor = System.Drawing.Color.LightGray;
+            this.Nota4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Nota4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota4.ForeColor = System.Drawing.Color.Blue;
+            this.Nota4.Location = new System.Drawing.Point(170, 9);
+            this.Nota4.Name = "Nota4";
+            this.Nota4.Size = new System.Drawing.Size(20, 22);
+            this.Nota4.TabIndex = 7;
+            this.Nota4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota5
+            // 
+            this.Nota5.BackColor = System.Drawing.Color.LightGray;
+            this.Nota5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Nota5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota5.ForeColor = System.Drawing.Color.Blue;
+            this.Nota5.Location = new System.Drawing.Point(196, 9);
+            this.Nota5.Name = "Nota5";
+            this.Nota5.Size = new System.Drawing.Size(20, 22);
+            this.Nota5.TabIndex = 8;
+            this.Nota5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota6
+            // 
+            this.Nota6.BackColor = System.Drawing.Color.LightGray;
+            this.Nota6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Nota6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota6.ForeColor = System.Drawing.Color.Blue;
+            this.Nota6.Location = new System.Drawing.Point(222, 9);
+            this.Nota6.Name = "Nota6";
+            this.Nota6.Size = new System.Drawing.Size(20, 22);
+            this.Nota6.TabIndex = 9;
+            this.Nota6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota7
+            // 
+            this.Nota7.BackColor = System.Drawing.Color.LightGray;
+            this.Nota7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Nota7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota7.ForeColor = System.Drawing.Color.Blue;
+            this.Nota7.Location = new System.Drawing.Point(248, 9);
+            this.Nota7.Name = "Nota7";
+            this.Nota7.Size = new System.Drawing.Size(20, 22);
+            this.Nota7.TabIndex = 10;
+            this.Nota7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Nota8
+            // 
+            this.Nota8.BackColor = System.Drawing.Color.LightGray;
+            this.Nota8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Nota8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Nota8.ForeColor = System.Drawing.Color.Blue;
+            this.Nota8.Location = new System.Drawing.Point(274, 9);
+            this.Nota8.Name = "Nota8";
+            this.Nota8.Size = new System.Drawing.Size(20, 22);
+            this.Nota8.TabIndex = 11;
+            this.Nota8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Teza
+            // 
+            this.Teza.BackColor = System.Drawing.Color.LightGray;
+            this.Teza.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Teza.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Teza.ForeColor = System.Drawing.Color.Blue;
+            this.Teza.Location = new System.Drawing.Point(92, 45);
+            this.Teza.Name = "Teza";
+            this.Teza.Size = new System.Drawing.Size(20, 22);
+            this.Teza.TabIndex = 12;
+            this.Teza.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Medie
+            // 
+            this.Medie.AutoSize = true;
+            this.Medie.BackColor = System.Drawing.Color.Black;
+            this.Medie.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Medie.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Medie.ForeColor = System.Drawing.Color.Red;
+            this.Medie.Location = new System.Drawing.Point(206, 41);
+            this.Medie.Name = "Medie";
+            this.Medie.Size = new System.Drawing.Size(0, 26);
+            this.Medie.TabIndex = 13;
+            this.Medie.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label3.Location = new System.Drawing.Point(118, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 26);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Medie:";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Coral;
+            this.label4.Location = new System.Drawing.Point(26, 301);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Designed and created by";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Brush Script MT", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Coral;
+            this.label5.Location = new System.Drawing.Point(189, 297);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 23);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Prodan Valeriu";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Coral;
+            this.label6.Location = new System.Drawing.Point(103, 321);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 14);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "For personal use only";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(313, 343);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Medie);
+            this.Controls.Add(this.Teza);
+            this.Controls.Add(this.Nota8);
+            this.Controls.Add(this.Nota7);
+            this.Controls.Add(this.Nota6);
+            this.Controls.Add(this.Nota5);
+            this.Controls.Add(this.Nota4);
+            this.Controls.Add(this.Nota3);
+            this.Controls.Add(this.Nota2);
+            this.Controls.Add(this.Nota1);
+            this.Controls.Add(this.Sugestii);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Medii";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Sugestii;
+        private System.Windows.Forms.TextBox Nota1;
+        private System.Windows.Forms.TextBox Nota2;
+        private System.Windows.Forms.TextBox Nota3;
+        private System.Windows.Forms.TextBox Nota4;
+        private System.Windows.Forms.TextBox Nota5;
+        private System.Windows.Forms.TextBox Nota6;
+        private System.Windows.Forms.TextBox Nota7;
+        private System.Windows.Forms.TextBox Nota8;
+        private System.Windows.Forms.TextBox Teza;
+        private System.Windows.Forms.Label Medie;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //double n ,i,t;
+            //n = Convert.ToDouble(Nota1.Text); i = Convert.ToDouble(Nota2.Text);t=Convert.ToDouble(Teza.Text) ; 
+            //label1.Text = Convert.ToString(Math.Round( (n/i*3+t)/4, MidpointRounding.AwayFromZero));
+
+            Sugestii.Text = "";
+            double n=0,a,s=0,t,m; 
+            int minim=10;
+            if (Nota1.Text != "") { n++; a = Convert.ToDouble(Nota1.Text); s = s + a; if (a < minim) minim = Convert.ToInt32(a); }
+            if (Nota2.Text != "") { n++; a = Convert.ToDouble(Nota2.Text); s = s + a; if (a < minim) minim = Convert.ToInt32(a); }
+            if (Nota3.Text != "") { n++; a = Convert.ToDouble(Nota3.Text); s = s + a; if (a < minim) minim = Convert.ToInt32(a); }
+            if (Nota4.Text != "") { n++; a = Convert.ToDouble(Nota4.Text); s = s + a; if (a < minim) minim = Convert.ToInt32(a); }
+            if (Nota5.Text != "") { n++; a = Convert.ToDouble(Nota5.Text); s = s + a; if (a < minim) minim = Convert.ToInt32(a); }
+            if (Nota6.Text != "") { n++; a = Convert.ToDouble(Nota6.Text); s = s + a; if (a < minim) minim = Convert.ToInt32(a); }
+            if (Nota7.Text != "") { n++; a = Convert.ToDouble(Nota7.Text); s = s + a; if (a < minim) minim = Convert.ToInt32(a); }
+            if (Nota8.Text != "") { n++; a = Convert.ToDouble(Nota8.Text); s = s + a; if (a < minim) minim = Convert.ToInt32(a); }
+
+            if (Teza.Text != "" )
+            {
+                t = Convert.ToDouble(Teza.Text);
+                m = Math.Round((s / n * 3 + t) / 4, MidpointRounding.AwayFromZero);
+                double m3=m;
+                for (int i = minim; i <= 10; i++)
+                {
+                    double m2;
+                    m2 = Math.Round(((s+i) / (n+1) * 3 + t) / 4, MidpointRounding.AwayFromZero);
+                    if (m2 > m3)
+                    {
+                        Sugestii.Text = Sugestii.Text + "+ " + Convert.ToString(i) + " => " + Convert.ToString(m2) + '\n';
+                        m3 = m2;
+                    }
+                }
+                for (int i = minim; i <= 10; i++)
+                    for (int j =i ; j <= 10; j++)
+                    {
+                        double m2;
+                        m2 = Math.Round(((s + i +j) / (n + 2) * 3 + t) / 4, MidpointRounding.AwayFromZero);
+                        if (m2 > m3)
+                        {
+                            Sugestii.Text = Sugestii.Text + "+ " + Convert.ToString(i) + " + " + Convert.ToString(j) +  " => " + Convert.ToString(m2) + '\n';
+                            m3 = m2;
+                        }
+                    }
+                for (int i = minim; i <= 10; i++)
+                    for (int j =i ; j <= 10; j++)
+                        for (int k =j ; k <= 10; k++)
+                        {
+                            double m2;
+                            m2 = Math.Round(((s + i + j+k) / (n + 3) * 3 + t) / 4, MidpointRounding.AwayFromZero);
+                            if (m2 > m3)
+                            {
+                                Sugestii.Text = Sugestii.Text + "+ " + Convert.ToString(i) + " + " + Convert.ToString(j) + " + " + Convert.ToString(k) + " => " + Convert.ToString(m2) + '\n';
+                                m3 = m2;
+                            }
+                        }
+            }
+            else
+            {
+                m = Math.Round(s / n , MidpointRounding.AwayFromZero);
+                double m3 = m;
+                for (int i = minim; i <= 10; i++)
+                {
+                    double m2;
+                    m2 = Math.Round((s + i) / (n + 1), MidpointRounding.AwayFromZero);
+                    if (m2 > m3)
+                    {
+                        Sugestii.Text = Sugestii.Text + "+ " + Convert.ToString(i) + " => " + Convert.ToString(m2) + '\n';
+                        m3 = m2;
+                    }
+                }
+                for (int i = minim; i <= 10; i++)
+                    for (int j = i; j <= 10; j++)
+                    {
+                        double m2;
+                        m2 = Math.Round((s + i + j) / (n + 2) , MidpointRounding.AwayFromZero);
+                        if (m2 > m3)
+                        {
+                            Sugestii.Text = Sugestii.Text + "+ " + Convert.ToString(i) + " + " + Convert.ToString(j) + " => " + Convert.ToString(m2) + '\n';
+                            m3 = m2;
+                        }
+                    }
+                for (int i = minim; i <= 10; i++)
+                    for (int j = i; j <= 10; j++)
+                        for (int k = j; k <= 10; k++)
+                        {
+                            double m2;
+                            m2 = Math.Round((s + i + j + k) / (n + 3), MidpointRounding.AwayFromZero);
+                            if (m2 > m3)
+                            {
+                                Sugestii.Text = Sugestii.Text + "+ " + Convert.ToString(i) + " + " + Convert.ToString(j) + " + " + Convert.ToString(k) + " => " + Convert.ToString(m2) + '\n';
+                                m3 = m2;
+                            }
+                        }
+            }
+            Medie.Text = Convert.ToString(m);
+            
+            //decimal n; n = 30.5m;
+            //label1.Text = "sdfsdfs df" + '\n' + "sdfgdf";//Convert.ToString(Math.Round(34.5,MidpointRounding.AwayFromZero));
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
